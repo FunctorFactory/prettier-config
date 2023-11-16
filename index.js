1de8dbf1e411cb0f967ed39bb4d2ad2d209dc10e
Jake Shilling <shilling.jake@gmail.com>
2023-11-15 19:11:33 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/** @type {import("prettier").Options} */
+const config = {
+  plugins: [],
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+};
+
+module.exports = config;
